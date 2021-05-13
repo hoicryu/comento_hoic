b23a6275235f525e16ac3c70bfed04d3ca5d6d5c
hoicryu <hoicryu@gmail.com>
2021-05-13 16:31:25 +0900

    [add] FeedDetail

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import feedMain from './pages/feed/FeedMain/FeedMain';
-import feedDetail from './pages/feed/FeedDetail/FeedDetail';
+import FeedMain from './pages/feed/FeedMain/FeedMain';
+import FeedDetail from './pages/feed/FeedDetail/FeedDetail';
 
 function Routes() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={feedMain} />
-        <Route exact path="/detail/:id" component={feedDetail} />
+        <Route exact path="/" component={FeedMain} />
+        <Route exact path="/detail/:id" component={FeedDetail} />
       </Switch>
     </Router>
   );
